8d01e0d50b7d044059922a6879fce91eb7aa7ccd
Elvencedor <dessy.eno25@gmail.com>
2020-10-12 13:18:03 +0100

    chore: add data signing methods to protector class

--- cryptolib/protector.cs
+++ cryptolib/protector.cs
@@ -14,6 +14,7 @@ namespace cryptolib
     private static readonly byte[] salt = Encoding.Unicode.GetBytes("SOMETHING");
     public static readonly int iterations = 2000;
     private static Dictionary<string, User> Users = new Dictionary<string, User>();
+    public static string publicKey;
 
     public static User Register (string username, string password) {
 
@@ -101,6 +102,58 @@ namespace cryptolib
       }
 
       return Encoding.Unicode.GetString(plainBytes);
+    }
+
+    public static string ToXmlStringExt (this RSA rsa, bool includePrivateParameters) {
+        var p = rsa.ExportParameters(includePrivateParameters);
+        XElement xml;
+
+        if (includePrivateParameters) {
+            xml =  new XElement("RSAKeyValue", 
+                new XElement("Modulus", ToBase64String(p.Modulus)),
+                new XElement("Exponent", ToBase64String(p.Exponent)),
+                new XElement("P", ToBase64String(p.P)),
+                new XElement("DQ", ToBase64String(p.DQ)),
+                new XElement("DP", ToBase64String(p.DP)),
+                new XElement("Q", ToBase64String(p.Q)),
+                new XElement("InverseQ", ToBase64String(p.InverseQ))
+            );
+        } else {
+            xml = new XElement("RSAKeyValue", 
+                new XElement("Modulus", ToBase64String(p.Modulus)),
+                new XElement("Exponent", ToBase64String(p.Exponent))
+            );
+        }
+
+        return xml?.ToString();
+    }
+
+    public static void FromXmlStringExt (this RSA rsa, string parametersAsXml) {
+        var xml = XDocument.Parse(parametersAsXml);
+        var root = xml.Element("RSAKeyValue");
+        var p = new RSAParameters {
+            Modulus = FromBase64String(root.Element("Modulus").Value),
+            Exponent = FromBase64String(root.Element("Exponent").Value)
+        };
+
+        if (root.Element("P") != null) {
+            p.P = FromBase64String(root.Element("P").Value);
+            p.Q = FromBase64String(root.Element("Q").Value);
+            p.DP = FromBase64String(root.Element("DP").Value);
+            p.DQ = FromBase64String(root.Element("DQ").Value);
+            p.InverseQ = FromBase64String(root.Element("InverseQ").Value);
+        }
+        rsa.ImportParameters(p);
+    }
+
+    public static string GenerateSignature (string data) {
+        byte[] dataBytes = Encoding.Unicode.GetBytes(data);
+        var sha = SHA256.Create();
+        var hashedData = sha.ComputeHash(dataBytes);
+        var rsa = RSA.Create();
+        publicKey = rsa.ToXmlStringExt(false);
+
+        return ToBase64String(rsa.SignHash(hashedData, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1));
     }
   }
 }
